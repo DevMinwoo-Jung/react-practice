452a4808d8fd1b3d3d30f63ba8aa3dadb9afd1ed
Minwoo <applejung312@gmail.com>
2022-01-22 22:01:15 +0900

    20220122

--- appleCoding/blog/src/App.js
+++ appleCoding/blog/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [title, setTitle] = useState([
+    "정민우 드디어 취업!!",
+    "정민우 드디어 정신차림!",
+    "연봉은 3천이상으로 밝혀져...!",
+  ]);
+
+  let [like, setLike] = useState(0);
+
+  function goLike(){
+    setLike(like + 1);
+  }
+
+  function goChangeTitle(){
+    const newTitle = [...title];
+    newTitle[0] = '코딩좀 하자..!';
+    setTitle(newTitle);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <div className="list">
+        <h3>{title[0]} <span onClick={goLike}>👍</span>{like}</h3>
+        <p>1월 21일</p>
+        <button onClick={goChangeTitle}>바꾸기</button>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{title[1]}</h3>
+        <p>1월 22일</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{title[2]}</h3>
+        <p>1월 23일</p>
+        <hr />
+      </div>
     </div>
   );
 }
